65103b7170fd653f6caac3f831ef5bed238a30f3
vinisdl <vinisdl@hotmail.com>
2024-06-06 11:43:58 -0300

    fix

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -35,17 +35,37 @@ export default function DataTable() {
 
             // Await make wait until that 
             // promise settles and return its result 
-            const response = await axios.get<Onlines>(
-                "https://rspv.azurewebsites.net/api/GetGuild?guild=Honbraland%20encore"
+            const response = await axios.get<TibiaData>(
+                "https://api.tibiadata.com/v4/guild/Honbraland%20encore"
             );
 
+
+
+
+            var online = response.data.guild.members.filter((item) => item.status === "online").slice().sort((p1, p2) =>
+                p1.level < p2.level ? 1
+                    : p1.level > p2.level ? -1
+                        : 0)
+
+
+            var onlines = {
+                kinight: online.filter((item) => item.vocation.includes("Knight")),
+                sorcerer: online.filter((item) => item.vocation.includes("Sorcerer")),
+                druid: online.filter((item) => item.vocation.includes("Druid")),
+                paladin: online.filter((item) => item.vocation.includes("Paladin"))
+            }
+
+
             // After fetching data stored it in posts state. 
-            setOnlines(response.data);
+            setOnlines(onlines);
 
         };
 
         // Call the function 
         loadOnlines();
+
+        setInterval(() => { loadOnlines(); }, 5000);
+
     }, []);
     return (
         <div style={{ height: 600, width: '100%' }}>
@@ -104,4 +124,70 @@ export default function DataTable() {
             />
         </div>
     );
+}
+
+
+export interface TibiaData {
+    guild: Guild
+    information: Information
+}
+
+export interface Guild {
+    active: boolean
+    description: string
+    disband_condition: string
+    disband_date: string
+    founded: string
+    guildhalls: Guildhall[]
+    homepage: string
+    in_war: boolean
+    invites: Invite[]
+    logo_url: string
+    members: Member[]
+    members_invited: number
+    members_total: number
+    name: string
+    open_applications: boolean
+    players_offline: number
+    players_online: number
+    world: string
+}
+
+export interface Guildhall {
+    name: string
+    paid_until: string
+    world: string
+}
+
+export interface Invite {
+    date: string
+    name: string
+}
+
+export interface Member {
+    joined: string
+    level: number
+    name: string
+    rank: string
+    status: string
+    title: string
+    vocation: string
+}
+
+export interface Information {
+    api: Api
+    status: Status
+    timestamp: string
+}
+
+export interface Api {
+    commit: string
+    release: string
+    version: number
+}
+
+export interface Status {
+    error: number
+    http_code: number
+    message: string
 }
